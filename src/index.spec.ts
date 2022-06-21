a3e1be69f60cd4dcb27f558fd292f7b42cdfd3e3
antoine-coulon <coulon_antoine@outlook.fr>
2022-06-22 00:17:52 +0200

    feat: allow cyclops to traverse ESM filesystem exploring import statements

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,95 +1,160 @@
+/* eslint-disable no-sync */
 import { expect } from "chai";
+import * as memfs from "memfs";
 
-import * as cyclops from "./index";
+import { Cyclops, FileReader } from "./index";
+
+export class InMemoryFileReader implements FileReader {
+  read(filename: string): Promise<string> {
+    return new Promise((resolve) => {
+      // @ts-expect-error - Will be a string as its decoded in utf-8
+      resolve(memfs.fs.readFileSync(filename, "utf-8"));
+    });
+  }
+}
 
 describe("When traversing a JavaScript project", () => {
   describe("When the project uses ECMAScript modules", () => {
     describe("When extracting import statements in the root file", () => {
-      describe("When the file does not have import statements", () => {
+      describe("When the file does not have any import statements", () => {
         it("cyclops should build a graph with one node", async () => {
-          const graph = await cyclops.buildGraph({
-            rootFile: "fixture/root-file.js"
-          })(() => Promise.resolve(`console.log(2)`));
+          const fakeFileSystem = {
+            "index.js": "console.log('Hello, world!');"
+          };
+          memfs.vol.fromJSON(fakeFileSystem, "./");
+
+          const cyclops = new Cyclops(
+            {
+              entrypoint: "index.js"
+            },
+            new InMemoryFileReader()
+          );
+
+          const graph = await cyclops.buildGraph();
+
           expect(graph).to.be.deep.equal({
             graph: {
-              "fixture/root-file.js": {
+              "index.js": {
                 adjacentTo: [],
-                id: "fixture/root-file.js",
+                id: "index.js",
                 payload: {}
               }
             },
-            files: ["fixture/root-file.js"],
-            cycles: [],
-            fileCount: 1
+            files: ["index.js"],
+            cycles: []
           });
         });
       });
 
       describe("When the file has one import statement", () => {
         it("cyclops should build the graph with two nodes and one edge", async () => {
-          const graph = await cyclops.buildGraph({
-            rootFile: "fixture/root-file.js"
-          })(() =>
-            Promise.resolve(`
-          import { foo } from "./foo.js";
-  
-          console.log(foo.doSomething());
-        `)
+          const fakeFileSystem = {
+            "index.js": `
+            import { foo } from "./src/foo.js";
+    
+            console.log(foo.doSomething());
+          `,
+            "src/foo.js": `
+              export const foo = { doSomething: () => 'Hello, world!' };
+            `
+          };
+
+          memfs.vol.fromJSON(fakeFileSystem, "./");
+
+          const cyclops = new Cyclops(
+            { entrypoint: "index.js" },
+            new InMemoryFileReader()
           );
+          const graph = await cyclops.buildGraph();
 
           expect(graph).to.be.deep.equal({
             graph: {
-              "fixture/root-file.js": {
-                adjacentTo: ["fixture/foo.js"],
-                id: "fixture/root-file.js",
+              "index.js": {
+                adjacentTo: ["src/foo.js"],
+                id: "index.js",
                 payload: {}
               },
-              "fixture/foo.js": {
+              "src/foo.js": {
                 adjacentTo: [],
-                id: "fixture/foo.js",
+                id: "src/foo.js",
                 payload: {}
               }
             },
-            files: ["fixture/root-file.js", "fixture/foo.js"],
-            cycles: [],
-            fileCount: 2
+            files: ["index.js", "src/foo.js"],
+            cycles: []
           });
         });
       });
 
-      describe("When the project has three import statements", () => {
-        it("cyclops should build the graph with three nodes and two edges", async () => {
-          const graph = await cyclops.buildGraph({
-            rootFile: "fixture/root-file.js"
-          })(() =>
-            Promise.resolve(`
-          import { foo } from "./foo.js";
-          import { bar } from "./bar.js";
-  
-          console.log(foo.doSomething());
-        `)
+      describe("When the project has four nested import statements", () => {
+        it("cyclops should build the graph with all nodes and edges", async () => {
+          const fakeFileSystem = {
+            "index.js": `
+              import { foo } from "./src/foo.js";
+              console.log(foo.doSomething());
+            `,
+            "src/foo.js": `
+              import { bar } from "./bar.js";
+              export const foo = { doSomething: () => bar() };
+            `,
+            "src/bar.js": `
+              import { baz } from './lib/baz/index.js';
+              export const bar = () => 'Hello, world!';
+            `,
+            "src/lib/baz/index.js": `
+              import lol from "../../lol/index.js";
+              export const baz = () => 'Hello, baz!';
+            `,
+            "src/lol/index.js": `
+              const lol = () => 'Hello, baz!';
+              export default lol;
+            `
+          };
+
+          memfs.vol.fromJSON(fakeFileSystem, "./");
+
+          const cyclops = new Cyclops(
+            { entrypoint: "index.js" },
+            new InMemoryFileReader()
           );
+          const graph = await cyclops.buildGraph();
+
           expect(graph).to.be.deep.equal({
             graph: {
-              "fixture/root-file.js": {
-                adjacentTo: ["fixture/foo.js", "fixture/bar.js"],
-                id: "fixture/root-file.js",
+              "index.js": {
+                adjacentTo: ["src/foo.js"],
+                id: "index.js",
                 payload: {}
               },
-              "fixture/foo.js": {
-                adjacentTo: [],
-                id: "fixture/foo.js",
+              "src/foo.js": {
+                adjacentTo: ["src/bar.js"],
+                id: "src/foo.js",
                 payload: {}
               },
-              "fixture/bar.js": {
+              "src/bar.js": {
+                adjacentTo: ["src/lib/baz/index.js"],
+                id: "src/bar.js",
+                payload: {}
+              },
+              "src/lib/baz/index.js": {
+                adjacentTo: ["src/lol/index.js"],
+                id: "src/lib/baz/index.js",
+                payload: {}
+              },
+              "src/lol/index.js": {
                 adjacentTo: [],
-                id: "fixture/bar.js",
+                id: "src/lol/index.js",
                 payload: {}
               }
             },
-            files: ["fixture/root-file.js", "fixture/foo.js", "fixture/bar.js"],
-            cycles: [],
-            fileCount: 3
+            files: [
+              "index.js",
+              "src/foo.js",
+              "src/bar.js",
+              "src/lib/baz/index.js",
+              "src/lol/index.js"
+            ],
+            cycles: []
           });
         });
       });
--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,114 @@
+import fs from "fs/promises";
 import path from "path";
 
 import { DiGraph } from "digraph-js";
 import { walk } from "estree-walker";
 import { parseScript } from "meriyah";
 
-function extractImportStatementsFromFile(fileContent: string): Array<string> {
-  const program = parseScript(fileContent, { module: true });
-
-  const importIdentifiers = new Set<string>();
-
-  walk(program.body, {
-    enter(node) {
-      if (node.type === "ImportDeclaration") {
-        importIdentifiers.add(node.source.value);
-      }
-    }
-  });
-
-  return [...importIdentifiers];
+interface CyclopsConfig {
+  entrypoint: string;
 }
 
-export function buildGraph({ rootFile }: { rootFile: string }) {
-  return async function openFile(
-    readRootFile: (file: string) => Promise<string>
-  ): Promise<any> {
-    const graph = new DiGraph();
-    graph.addVertices({ id: rootFile, adjacentTo: [], payload: {} });
+export interface FileReader {
+  read: (filename: string) => Promise<string>;
+}
 
-    const rootFileContent = await readRootFile(rootFile);
-    const importStatementsFromFile =
-      extractImportStatementsFromFile(rootFileContent);
+export class FileSystemReader implements FileReader {
+  read(filename: string): Promise<string> {
+    return fs.readFile(filename, "utf-8");
+  }
+}
 
-    for (const fileImport of importStatementsFromFile) {
-      const relativePath = path.join(path.dirname(rootFile), fileImport);
-      graph.addVertices({ id: relativePath, adjacentTo: [], payload: {} });
-      graph.addEdge({
-        from: { id: rootFile, adjacentTo: [], payload: {} },
-        to: { id: relativePath, adjacentTo: [], payload: {} }
-      });
+export class Cyclops {
+  #projectGraph = new DiGraph();
+
+  constructor(
+    private readonly cyclopsConfig: CyclopsConfig,
+    private readonly fileReader: FileReader
+  ) {}
+
+  private addNode(node: string): void {
+    this.#projectGraph.addVertices({
+      id: node,
+      adjacentTo: [],
+      payload: {}
+    });
+  }
+
+  private linkNodes({ from, to }: { from: string; to: string }): void {
+    this.#projectGraph.addEdge({
+      from: {
+        id: from,
+        adjacentTo: [],
+        payload: {}
+      },
+      to: {
+        id: to,
+        adjacentTo: [],
+        payload: {}
+      }
+    });
+  }
+
+  private async traverseImportStatementsFromFile(
+    rootDir: string,
+    fileContent: string
+  ): Promise<void> {
+    const importIdentifiers = new Set<string>();
+    const node = parseScript(fileContent, { module: true });
+    const isRootNode = node.type === "Program";
+
+    walk(isRootNode ? node.body : node, {
+      enter(node) {
+        if (node.type === "ImportDeclaration") {
+          importIdentifiers.add(node.source.value);
+        }
+      }
+    });
+
+    if (importIdentifiers.size === 0) {
+      return;
     }
 
-    const fileList = Object.keys(graph.asObject());
+    for (const importIdentifier of importIdentifiers.values()) {
+      const fullFilePathFromEntrypoint = path.join(
+        path.dirname(rootDir),
+        importIdentifier
+      );
+
+      this.addNode(fullFilePathFromEntrypoint);
+      this.linkNodes({
+        from: rootDir,
+        to: fullFilePathFromEntrypoint
+      });
+
+      const nextFileToExplore = await this.fileReader.read(
+        fullFilePathFromEntrypoint
+      );
+
+      await this.traverseImportStatementsFromFile(
+        fullFilePathFromEntrypoint,
+        nextFileToExplore
+      );
+    }
+  }
+
+  async buildGraph(): Promise<any> {
+    this.addNode(this.cyclopsConfig.entrypoint);
+
+    const rootFileContent = await this.fileReader.read(
+      this.cyclopsConfig.entrypoint
+    );
+    const rootDirPath = this.cyclopsConfig.entrypoint;
+
+    await this.traverseImportStatementsFromFile(rootDirPath, rootFileContent);
+
+    const fileList = Object.keys(this.#projectGraph.asObject());
 
     return {
-      graph: graph.asObject(),
+      graph: this.#projectGraph.asObject(),
       files: fileList,
-      cycles: [],
-      fileCount: fileList.length
+      cycles: []
     };
-  };
+  }
 }
