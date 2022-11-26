64be318f0fe45d10904d905f87cc82199bb82c22
antoine-coulon <coulon_antoine@outlook.fr>
2022-11-26 12:41:49 +0100

    feat: automatically open browser when usign webapp display mode

--- packages/skott/bin/ui/webapp.ts
+++ packages/skott/bin/ui/webapp.ts
@@ -4,45 +4,62 @@ import url from "node:url";
 
 import compression from "compression";
 import kleur from "kleur";
+import { open } from "openurl";
 import polka from "polka";
 import sirv from "sirv";
 
 import { SkottInstance, SkottStructure } from "../../src/skott.js";
 
-export function openWebApplication(
-  skottInstance: SkottInstance,
-  skottStructure: SkottStructure
-): void {
+function findSkottWebAppDirectory(): string {
   const __dirname = path.dirname(url.fileURLToPath(new URL(import.meta.url)));
-  const baseSkottWebAppDirectory = path.join(
-    __dirname,
-    "..",
-    "..",
+  const rootSkottDirectory = path.join(__dirname, "..", "..", "..");
+  const skottWebAppDirectory = path.join(
     "node_modules",
     "@skott",
     "webapp",
     "dist"
   );
 
+  return path.join(rootSkottDirectory, skottWebAppDirectory);
+}
+
+export function openWebApplication(
+  skottInstance: SkottInstance,
+  skottStructure: SkottStructure
+): void {
+  const skottWebAppPath = findSkottWebAppDirectory();
+
   const compress = compression();
-  const assets = sirv(baseSkottWebAppDirectory, {
+  const assets = sirv(skottWebAppPath, {
     immutable: true
   });
-  const port = 49389;
 
-  polka()
+  const srv = polka()
     .use(compress, assets)
-    .use("/api", (_: any, response: ServerResponse) => {
+    .use("/api", (_, response: ServerResponse) => {
       const cycles = skottInstance.findCircularDependencies();
 
       response.setHeader("Content-Type", "application/json");
       response.end(JSON.stringify({ ...skottStructure, cycles }));
     })
-    .listen(port, () => {
+    .listen(process.env.SKOTT_PORT || 0);
+
+  // @ts-expect-error - "port" exists
+  const bindedAddress = `http://127.0.0.1:${srv.server?.address()?.port}`;
+
+  console.log(
+    `\n ${kleur.bold("💻 Opened @skott/webapp on")} ${kleur
+      .bold()
+      .bgMagenta(`${bindedAddress}`)}`
+  );
+
+  open(bindedAddress, (error) => {
+    if (error) {
       console.log(
-        `\n ${kleur.bold("💻 Opened Skott web app on")} ${kleur
-          .bold()
-          .bgMagenta(`http://127.0.0.1:${port}`)}`
+        `\n ${kleur
+          .red()
+          .bold(`Could not open @skott/webapp on ${bindedAddress}`)}`
       );
-    });
+    }
+  });
 }
