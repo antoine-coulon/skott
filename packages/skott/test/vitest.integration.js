a2421629b09b519438e171ff93a7e6624dbcc55b
antoine-coulon <coulon_antoine@outlook.fr>
2023-06-23 17:22:44 +0200

    test: update vitest config

--- packages/skott/test/vitest.integration.js
+++ packages/skott/test/vitest.integration.js
@@ -3,6 +3,8 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     include: ["./test/integration/**/*.spec.ts"],
-    logHeapUsage: true
+    logHeapUsage: true,
+    maxThreads: 1,
+    minThreads: 1
   }
 });
