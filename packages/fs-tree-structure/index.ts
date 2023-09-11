2853c931e3201b4fc6f872e71a8e994584e46757
Antoine Coulon <43391199+antoine-coulon@users.noreply.github.com>
2023-09-11 12:54:53 +0200

    feat(fs-tree-structure): sort tree structure in order for folders to appear first instead (#85)
    * feat: support sort by file and folder for fs-tree-structure
    
    * fs-tree-structure: 0.0.2

--- packages/fs-tree-structure/index.ts
+++ packages/fs-tree-structure/index.ts
@@ -4,12 +4,57 @@ import set from "lodash.set";
 
 export type TreeStructure = { [key: string]: TreeStructure };
 
+// @TODO: should be mutualized with the one in skott
+const fileExtensions = [".js", ".jsx", ".ts", ".tsx", ".mjs", ".cjs"];
+
 function isNodeJSRuntime(): boolean {
   return typeof process !== "undefined";
 }
 
-export function makeTreeStructure(filePaths: string[]): TreeStructure {
-  return filePaths.reduce((tree, filePath) => {
+function isFile(filePath: string): boolean {
+  return fileExtensions.some((extension) => filePath.endsWith(extension));
+}
+
+function sortByResourceType(record: TreeStructure) {
+  const sortedTree: TreeStructure = {};
+  const sortedKeys = Object.keys(record).sort((a, b) => {
+    if (isFile(a)) {
+      if (isFile(b)) {
+        return a.localeCompare(b);
+      } else {
+        return 1;
+      }
+    }
+
+    return -1;
+  });
+
+  for (const sortedKey of sortedKeys) {
+    const value = record[sortedKey];
+
+    sortedTree[sortedKey] = value;
+  }
+
+  return sortedTree;
+}
+
+function sortTree(root: TreeStructure) {
+  const newTree: TreeStructure = {};
+
+  for (const [key, value] of Object.entries(root)) {
+    newTree[key] = sortTree(value);
+  }
+
+  return sortByResourceType(newTree);
+}
+
+export function makeTreeStructure(
+  filePaths: string[],
+  options = {
+    sort: false
+  }
+): TreeStructure {
+  const fileTree = filePaths.reduce((tree, filePath) => {
     set(
       tree,
       filePath
@@ -20,4 +65,10 @@ export function makeTreeStructure(filePaths: string[]): TreeStructure {
 
     return tree;
   }, {});
+
+  if (options.sort) {
+    return sortTree(fileTree);
+  }
+
+  return fileTree;
 }
