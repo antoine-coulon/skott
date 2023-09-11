efd9c184dd36a1f4e091c3cd266515820dc39ec2
Alexis CHAPPRON <bullrot-10@hotmail.fr>
2023-09-11 21:35:20 +0200

    fix: remove sade in favor of commander to actually allow 2 character option aliases

--- packages/skott/bin/cli.ts
+++ packages/skott/bin/cli.ts
@@ -4,7 +4,7 @@ import fs from "node:fs";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 
-import sade from "sade";
+import { Command } from 'commander';
 
 import { kExpectedModuleExtensions } from "../src/modules/resolvers/base-resolver.js";
 
@@ -28,113 +28,74 @@ function readManifestVersion(): string {
   }
 }
 
-const cli = sade("skott [entrypoint]", true)
-  .version(readManifestVersion())
+/**
+ * Parses a string into an integer value.
+ * @param {string} value - The string to be parsed into an integer.
+ * @return {number} - The parsed integer value.
+ * @throws {Error} - Throws an error if the given value is not a number.
+ */
+function commanderParseInt(value: string): number {
+  // parseInt takes a string and a radix
+  const parsedValue = parseInt(value, 10);
+  if (isNaN(parsedValue)) {
+    throw new Error('Not a number.');
+  }
+  return parsedValue;
+}
 
-  .describe("Start the Skott analysis to fully build the graph")
+/**
+ * Function similar to kotlin's trimMargin
+ * This is really helpful to print multiline string while keeping code indentation readable
+ * Example:
+ * trimMargin('|')(`
+ *     |this is a super long multiline string
+ *     |defined on multiple lines
+ *     |while well formatted in the code
+ * `)
+ * will return:
+ * this is a super long multiline string
+ * defined on multiple lines
+ * while well formatted in the code
+ * @param delimiter
+ */
+export function trimMargin(delimiter = '|'): (str: string) => string {
+  const reg = new RegExp(`^\\s*\\${delimiter}`, 'g');
+  return (str: string): string =>
+      str
+          .split('\n')
+          .map((line) => line.replace(reg, ''))
+          .join('\n');
+}
 
-  .option(
-    "-b, --includeBaseDir",
-    "Include the base directory name for each graph node. Relative to the entrypoint base directory or the current working directory.",
-    false
-  )
+const cli = new Command();
 
-  .option(
-    "-c, --exitCodeOnCircularDependencies",
-    "Specify the exit code to use when circular dependencies are found",
-    1
-  )
+cli
+    .version(readManifestVersion())
+    .description('Start the Skott analysis to fully build the graph')
+    .option('-b, --includeBaseDir', 'Include the base directory name for each graph node. Relative to the entrypoint base directory or the current working directory.', false)
+    .option('-c, --exitCodeOnCircularDependencies', 'Specify the exit code to use when circular dependencies are found', commanderParseInt, 1)
+    .option('-d, --circularMaxDepth', 'Define the max depth of the nested circular dependencies search', commanderParseInt, Number.POSITIVE_INFINITY)
+    .option('-e, --fileExtensions', 'File extensions to explore when building the graph', [...kExpectedModuleExtensions].join(","))
+    .option('-m, --displayMode', 'Either display the result of the analysis as a graph, as a file-tree or raw', 'webapp')
+    .option('-n, --trackBuiltinDependencies', 'Enable Node.js builtin dependency tracking', false)
+    .option('-i, --incremental', '(Experimental) Enable incremental mode. Only the files that changed since the last run will be analyzed.', false)
+    .option('-it, --trackTypeOnlyDependencies', 'Enable dependency tracking for Typescript import type statements', false)
+    .option('-ig, --ignorePattern', 'Provide ignore pattern to exclude files from the analysis', '')
+    .option('-s, --showCircularDependencies', 'Show all circular dependencies in the graph', false)
+    .option('-ts, --tsconfig', 'Provide a path to a tsconfig.json file to use for path aliases resolution', 'tsconfig.json')
+    .option('-mf, --manifest', 'Provide a path to a manifest file used for dependencies resolution', 'package.json')
+    .option('-t, --trackThirdPartyDependencies', 'Enable npm third-party dependency tracking', false)
+    .option('-u, --showUnusedDependencies', 'Search for unused third-party dependencies in the graph', false)
+    .option('-vb, --verbose', 'Enable verbose mode. Display all the logs', false)
+    .option('-w, --cwd', 'Define the base working directory to use for the analysis. Defaults to the current working directory.', process.cwd())
+    .argument('[entrypoint]', 'optional entrypoint file to use')
+    .usage(
+        trimMargin('|')(`
+          | ./node_modules/.bin/skott src/index.js --displayMode=file-tree --staticFile=md\n
+          | ./node_modules/.bin/skott  --fileExtensions=.ts,.tsx --tsconfig=tsconfig.base.json\n
+          | ./node_modules/.bin/skott --showCircularDependencies --displayMode=raw\n
+        `)
+    )
+    .action((name, commandAndOptions) => displaySkott(name, commandAndOptions));
 
-  .option(
-    "-d, --circularMaxDepth",
-    "Define the max depth of the nested circular dependencies search",
-    Number.POSITIVE_INFINITY
-  )
-
-  .option(
-    "-e, --fileExtensions",
-    "File extensions to explore when building the graph",
-    [...kExpectedModuleExtensions].join(",")
-  )
-
-  .option(
-    "-m, --displayMode",
-    "Either display the result of the analysis as a 'graph', as a 'file-tree' or 'raw'",
-    "webapp"
-  )
-
-  .option(
-    "-n, --trackBuiltinDependencies",
-    "Enable Node.js builtin dependency tracking",
-    false
-  )
-
-  .option(
-    "-i, --incremental",
-    "(Experimental) Enable incremental mode. Only the files that changed since the last run will be analyzed.",
-    false
-  )
-
-  .option(
-    "-it, --trackTypeOnlyDependencies",
-    "Enable dependency tracking for Typescript 'import type' statements",
-    true
-  )
-
-  .option(
-    "-ig, --ignorePattern",
-    "Provide ignore pattern to exclude files from the analysis",
-    ""
-  )
-
-  .option(
-    "-s, --showCircularDependencies",
-    "Show all circular dependencies in the graph",
-    false
-  )
-
-  .option(
-    "-ts, --tsconfig",
-    "Provide a path to a tsconfig.json file to use for path aliases resolution",
-    "tsconfig.json"
-  )
-
-  .option(
-    "-mf, --manifest",
-    "Provide a path to a manifest file used for dependencies resolution",
-    "package.json"
-  )
-
-  .option(
-    "-t, --trackThirdPartyDependencies",
-    "Enable npm third-party dependency tracking",
-    false
-  )
-
-  .option(
-    "-u, --showUnusedDependencies",
-    "Search for unused third-party dependencies in the graph",
-    false
-  )
-
-  .option("-v, --verbose", "Enable verbose mode. Display all the logs", false)
-
-  .option(
-    "-w, --cwd",
-    "Define the base working directory to use for the analysis. Defaults to the current working directory.",
-    process.cwd()
-  )
-
-  .example(
-    "./node_modules/.bin/skott src/index.js --displayMode=file-tree --staticFile=md"
-  )
-  .example(
-    "./node_modules/.bin/skott  --fileExtensions=.ts,.tsx --tsconfig=tsconfig.base.json"
-  )
-  .example(
-    "./node_modules/.bin/skott --showCircularDependencies --displayMode=raw"
-  )
-
-  .action(displaySkott);
-
-cli.parse(process.argv, { unknown: (flag) => `Unknown option: ${flag}` });
+cli.parse(process.argv);
