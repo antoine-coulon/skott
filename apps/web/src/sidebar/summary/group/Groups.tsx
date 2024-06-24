feb29e94ccf8e2c46dbdad5edb0ca177f6d8613d
antoine-coulon <coulon_antoine@outlook.fr>
2024-06-24 08:13:55 +0200

    feat: simple groups summary

--- apps/web/src/sidebar/summary/group/Groups.tsx
+++ apps/web/src/sidebar/summary/group/Groups.tsx
@@ -1,6 +1,16 @@
 import { useStoreSelect } from "@/store/react-bindings";
-import { Badge, Flex, Navbar, Paper, ScrollArea, Text } from "@mantine/core";
+import {
+  Badge,
+  Box,
+  Flex,
+  List,
+  Navbar,
+  Paper,
+  ScrollArea,
+  Text,
+} from "@mantine/core";
 import * as Option from "@effect/data/Option";
+import { IconChevronDownLeft, IconHexagons } from "@tabler/icons-react";
 
 export function Groups() {
   const maybeGraph = useStoreSelect("data", "graph");
@@ -26,6 +36,46 @@ export function Groups() {
             </Badge>
           </Flex>
         </Paper>
+
+        <Flex p="sm" justify="start" align="center" direction="row">
+          <Box p={10}>
+            <List
+              spacing="xs"
+              size="sm"
+              icon={
+                <IconHexagons
+                  size={15}
+                  style={{
+                    marginTop: "0.2rem",
+                  }}
+                />
+              }
+            >
+              {Object.values(graph).map(({ id, adjacentTo }) => {
+                return (
+                  <List.Item key={id} mb="md">
+                    <Text weight="bold">{id}</Text>
+
+                    {adjacentTo.map((adjacentId) => {
+                      return (
+                        <Flex>
+                          <IconChevronDownLeft color="orange" />
+                          <Text
+                            style={{
+                              marginTop: "0.2rem",
+                            }}
+                          >
+                            {adjacentId}
+                          </Text>
+                        </Flex>
+                      );
+                    })}
+                  </List.Item>
+                );
+              })}
+            </List>
+          </Box>
+        </Flex>
       </Navbar.Section>
     </ScrollArea.Autosize>
   );
